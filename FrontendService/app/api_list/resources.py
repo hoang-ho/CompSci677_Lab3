8f78937cd8998a1b3fbd0407658e96bcae34aa0a
JB <jpbhavsar08@gmail.com>
2021-04-20 05:21:25 -0400

    implemented in-memory caching functionality

--- FrontendService/app/api_list/resources.py
+++ FrontendService/app/api_list/resources.py
@@ -25,12 +25,17 @@ ORDER_HOST = os.getenv('ORDER_HOST')
 ORDER_PORT = os.getenv('ORDER_PORT')
 
 
+# Local caching
+cache = dict()
+
+
 class Search(Resource):
     '''
     Handle search by topic request
     '''
 
     t_start = time.time()
+    t_end = 0
     def get(self, topic_name=None):
         # return the expected value
         if not topic_name:
@@ -46,21 +51,32 @@ class Search(Resource):
         elif(topic_name == 'graduate-school'):
            data = {"topic": "graduate school"}
         else:
-            t_end = time.time()
-            logger.info(f'execution time for search: {t_end-self.t_start}')
+            self.t_end = time.time()
+            logger.info(f'execution time for search: {self.t_end-self.t_start}')
             return {"message": "topic name should be in [distributed-systems, graduate-school]"}, 400     
         
         # requesting to catalog
         try:
-            logger.info("catalog host: ", CATALOG_HOST)
-            response = requests.get(f'http://{CATALOG_HOST}:{CATALOG_PORT}/catalog/query', json=data)
-            if response.status_code == 200:
-                t_end = time.time()
-                logger.info(f'execution time for search: {t_end-self.t_start}')
-                return response.json()
+            logger.info(f'Catalog Host: {CATALOG_HOST}')
+
+            target_key = f'search-{topic_name}'
+            logger.info(f'target key: {target_key}')
+            if target_key not in cache:
+                logger.info(f'calling the backend server')
+                response = requests.get(f'http://{CATALOG_HOST}:{CATALOG_PORT}/catalog/query', json=data)
+                if response.status_code == 200:
+                    self.t_end = time.time()
+                    logger.info(f'execution time for search: {self.t_end-self.t_start}')
+                    cache[target_key] = response.json()
+            
+            cached_response = cache[target_key]
+            logger.info(f'returning cached response')
+            self.t_end = time.time()
+            logger.info(f'execution time for search: {self.t_end-self.t_start}')
+            return cached_response, 200
         except:
-            t_end = time.time()
-            logger.info(f'execution time for search: {t_end-self.t_start}')
+            self.t_end = time.time()
+            logger.info(f'execution time for search: {self.t_end-self.t_start}')
             return {'message': 'something went wrong. Please try again'}, 500
 
 
@@ -70,6 +86,7 @@ class LookUp(Resource):
     '''
 
     t_start = time.time()
+    t_end = 0
     def get(self, item_id=None):
         # return the expected value
         if not item_id:
@@ -82,22 +99,32 @@ class LookUp(Resource):
         # validation
         id = int(item_id)
         if(id > 4 or id < 1):
-            t_end = time.time()
-            logger.info(f'execution time for lookup: {t_end-self.t_start}')
+            self.t_end = time.time()
+            logger.info(f'execution time for lookup: {self.t_end-self.t_start}')
             return {"message": "Please enter a correct id"}, 400  
 
         data = {"id": id}
 
         # requesting to catalog
         try:
-            response = requests.get(f'http://{CATALOG_HOST}:{CATALOG_PORT}/catalog/query', json=data)
-            if response.status_code == 200:
-                t_end = time.time()
-                logger.info(f'execution time for lookup: {t_end-self.t_start}')
-                return response.json()
+            target_key = f'lookup-{item_id}'
+            logger.info(f'target key: {target_key}')
+            if target_key not in cache:
+                logger.info(f'calling the backend server')
+                response = requests.get(f'http://{CATALOG_HOST}:{CATALOG_PORT}/catalog/query', json=data)
+                if response.status_code == 200:
+                    self.t_end = time.time()
+                    logger.info(f'execution time for lookup: {self.t_end-self.t_start}')
+                    cache[target_key] = response.json()
+
+            cached_response = cache[target_key]
+            logger.info(f'returning cached response')
+            self.t_end = time.time()
+            logger.info(f'execution time for search: {self.t_end-self.t_start}')
+            return cached_response, 200
         except:
-            t_end = time.time()
-            logger.info(f'execution time for lookup: {t_end-self.t_start}')
+            self.t_end = time.time()
+            logger.info(f'execution time for lookup: {self.t_end-self.t_start}')
             return {'message': 'something went wrong. Please try again'}, 500
         
 
@@ -107,6 +134,7 @@ class Buy(Resource):
     '''
 
     t_start = time.time()
+    t_end = 0
     def post(self, item_id=None):
         # return the expected value
         if not item_id:
@@ -119,8 +147,8 @@ class Buy(Resource):
         # validation
         id = int(item_id)
         if(id > 4 or id < 1):
-            t_end = time.time()
-            logger.info(f'execution time for buy: {t_end-self.t_start}')
+            self.t_end = time.time()
+            logger.info(f'execution time for buy: {self.t_end-self.t_start}')
             return {"message": "Please enter a correct id"}, 400  
 
         data = {"id": id}
@@ -129,14 +157,14 @@ class Buy(Resource):
         try:
             response = requests.put(f'http://{ORDER_HOST}:{ORDER_PORT}/order', json=data)
             if response.status_code == 200:
-                t_end = time.time()
-                logger.info(f'execution time for buy: {t_end-self.t_start}')
+                self.t_end = time.time()
+                logger.info(f'execution time for buy: {self.t_end-self.t_start}')
                 response_json = response.json()
                 book = response_json.get('book', '')
                 if(book):
                     return {'message': f'successfully purchased the book {book}'}
                 return response.json()
         except:
-            t_end = time.time()
-            logger.info(f'execution time for buy: {t_end-self.t_start}')
+            self.t_end = time.time()
+            logger.info(f'execution time for buy: {self.t_end-self.t_start}')
             return {'message': 'something went wrong. Please try again'}, 500
